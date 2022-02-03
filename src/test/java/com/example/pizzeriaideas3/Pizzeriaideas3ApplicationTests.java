059b1fee309c70097f71da2e93aa14b4d12bddd4
Cristian Martinez <84696240+cristianmartinez06@users.noreply.github.com>
2022-02-03 10:40:31 -0300

    Add files via upload

---
+++ src/test/java/com/example/pizzeriaideas3/Pizzeriaideas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzeriaideas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pizzeriaideas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
