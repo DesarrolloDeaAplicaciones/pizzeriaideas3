25d8ea0bb1d0308501eaa78b3c8107023b79e4b0
sebastian de la cruz <sebastiandelacruz3352gmail.com>
2022-02-10 11:53:01 -0300

    tarea sebastian prototipo  finalizada

--- src/template/js/index.js
+++ src/template/js/index.js
@@ -1,24 +1,150 @@
 const doc = document;
 
-const $btnFill = doc.getElementById("btn-fill"),
-$btnPlus =  doc.getElementById("btn-plus"),
-$pantalla = doc.getElementById("pantalla");
 
-let contador = 0;
-doc.addEventListener("click",(event)=>{
+const $pizzas = doc.querySelectorAll("[data-pizza]"),
+$empanadas =  doc.querySelectorAll("[data-empanada]"),
+$bebidas =  doc.querySelectorAll("[data-bebida]"),
+$postres = doc.querySelectorAll("[data-postre]");
 
-    if(event.target.matches("#btn-plus")) contador++;
+
+$pizzas.forEach((i) =>{
+
+    let contador = 0;
+
+
+    i.addEventListener("click" ,(event)=>{
+
+        if(contador <= 0){
+
+            contador = 1;
+        }
         
-    if(event.target.matches("#btn-fill")) contador--;
 
-    if(contador <= 0){
+        if(event.target.matches("#btn-plus")){
+            
+            contador++;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
 
-        contador = 0;
+        }
+        
+        if(event.target.matches("#btn-fill")){
 
-    }
+            contador--;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
 
-    $pantalla.innerHTML = contador;
-    
+        }
 
-    
-}); 
+        
+    });
+
+
+});
+
+
+$empanadas.forEach((i) =>{
+
+    let contador = 0;
+
+
+    i.addEventListener("click" ,(event)=>{
+
+        if(contador <= 0){
+
+            contador = 1;
+        }
+        
+
+        if(event.target.matches("#btn-plus")){
+            
+            contador++;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+        
+        if(event.target.matches("#btn-fill")){
+
+            contador--;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+
+        
+    });
+
+
+});
+
+$bebidas.forEach((i) =>{
+
+    let contador = 0;
+
+
+    i.addEventListener("click" ,(event)=>{
+
+        if(contador <= 0){
+
+            contador = 1;
+        }
+        
+
+        if(event.target.matches("#btn-plus")){
+            
+            contador++;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+        
+        if(event.target.matches("#btn-fill")){
+
+            contador--;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+
+        
+    });
+
+
+});
+
+
+$postres.forEach((i) =>{
+
+    let contador = 0;
+
+
+    i.addEventListener("click" ,(event)=>{
+
+        if(contador <= 0){
+
+            contador = 1;
+        }
+        
+
+        if(event.target.matches("#btn-plus")){
+            
+            contador++;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+        
+        if(event.target.matches("#btn-fill")){
+
+            contador--;
+            
+            i.querySelector(".footer-container #pantalla").innerHTML = contador;
+
+        }
+
+        
+    });
+
+
+});
